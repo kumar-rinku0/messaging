98b85c0b2bd9ecac43e61994223fc0b2393cdcc3
kumar-rinku0 <rinkukumar412616@gmail.com>
2025-08-19 04:59:49 +0530

    socket io addded

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,14 +1,31 @@
 import express from "express";
-import { config } from "dotenv"; 
+import { config } from "dotenv";
+import {createServer} from "http";
+import {Server} from "socket.io"
 config();
 
-const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const app = express()
+const server = createServer(app);
+const io = new Server(server, {cors: "http://localhost:5173"});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('create-something', (msg) => {
+    console.log('create-something received:', msg);
+    io.emit('foo', msg);
+  });
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+server.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
