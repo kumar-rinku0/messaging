59bccceee32c03c3c4632fce08cbe76d6b7fa7b9
kumar-rinku0 <rinkukumar412616@gmail.com>
2025-08-23 05:43:58 +0530

    chat app code refined

--- front-end/src/components/private/chat-app.tsx
+++ front-end/src/components/private/chat-app.tsx
@@ -1,11 +1,9 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { socket } from "@/services/socket";
 import { Button } from "../ui/button";
 import OnlineUsers from "./online-users";
 
 const ChatApp = () => {
-  // const [isConnected, setIsConnected] = useState(socket.connected);
-  const [chatMessages, setChatMessages] = useState<any[]>([]);
   const token = localStorage.getItem("token");
 
   const handleLogout = () => {
@@ -20,51 +18,28 @@ const ChatApp = () => {
       socket.connect();
     };
 
-    function onConnect() {
-      console.log("Connected to socket server");
-      socket.emit("newUser", token);
-    }
-
-    function onDisconnect() {
-      console.log("Disconnected from socket server");
-    }
-
-    function onChatMessage(value: any) {
-      console.log("Received chat message:", value);
-      // chrome notification on chat message
-      if (Notification.permission !== "granted") {
-        Notification.requestPermission();
-      }
-      if (Notification.permission === "granted") {
-        new Notification("Chat Message", {
-          body: `Received chat message: ${value}`,
-        });
-      }
-      setChatMessages((previous) => [...previous, value]);
-    }
     onUsernameSelection();
-    socket.on("connect", onConnect);
-    socket.on("disconnect", onDisconnect);
-    socket.on("chat-message", onChatMessage);
-
-    return () => {
-      socket.off("connect", onConnect);
-      socket.off("disconnect", onDisconnect);
-      socket.off("chat-message", onChatMessage);
-    };
   }, []);
   return (
     <div>
       <h1>Chat Application</h1>
       <OnlineUsers />
       <Button onClick={handleLogout}>Logout</Button>
-      <div>
-        {chatMessages.map((msg, index) => (
-          <div key={index}>{msg}</div>
-        ))}
-      </div>
     </div>
   );
 };
 
 export default ChatApp;
+
+// function onChatMessage(value: any) {
+//   console.log("Received chat message:", value);
+//   // chrome notification on chat message
+//   if (Notification.permission !== "granted") {
+//     Notification.requestPermission();
+//   }
+//   if (Notification.permission === "granted") {
+//     new Notification("Chat Message", {
+//       body: `Received chat message: ${value}`,
+//     });
+//   }
+// }
