3573e83dd5c967ac26b678d8354315c210e2a6a7
kumar-rinku0 <rinkukumar412616@gmail.com>
2025-08-26 12:42:25 +0530

    distbution folder removed from git

--- back-end/dist/routes/msg.route.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const msg_controller_1 = require("../controllers/msg.controller");
-const msgRouter = (0, express_1.Router)();
-msgRouter.route("/").post(msg_controller_1.handleCreateMessage);
-msgRouter.route("/messages/:chatId").get(msg_controller_1.handleGetMessagesByChatId);
-msgRouter.route("/last-message/:chatId").get(msg_controller_1.handleGetLastMessageByChatId);
-exports.default = msgRouter;
--- back-end/dist/routes/user.route.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const user_controller_1 = require("../controllers/user.controller");
-const userRouter = (0, express_1.Router)();
-userRouter.route("/register").post(user_controller_1.handleUserRegistration);
-userRouter.route("/login").post(user_controller_1.handleUserLogin);
-userRouter.route("/all").get(user_controller_1.handleGetAllUsers); // Endpoint to get all users
-exports.default = userRouter;
