f080ac44852f3db8a3892857a432f2e73b1d878b
kumar-rinku0 <rinkukumar412616@gmail.com>
2025-08-27 19:12:11 +0530

    socket.ts updated with socket-url

--- front-end/src/services/socket.ts
+++ front-end/src/services/socket.ts
@@ -1,8 +1,9 @@
 import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
+const socketUrl = import.meta.env.VITE_API_URL || "http://localhost:4000";
 
-const socket = io({
+const socket = io(socketUrl, {
   path: "/api/socket.io",
   autoConnect: false,
 });
