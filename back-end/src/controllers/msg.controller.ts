71f5dbe867c11973b20e26214ee64a13e85d2a43
kumar-rinku0 <rinkukumar412616@gmail.com>
2025-10-21 13:53:48 +0530

    loading more chatting messages stuff updated & totalpages returned

--- back-end/src/controllers/msg.controller.ts
+++ back-end/src/controllers/msg.controller.ts
@@ -23,20 +23,24 @@ const handleGetMessagesByChatId = async (req: Request, res: Response) => {
   if (!chat) {
     return res.status(404).json({ message: "Chat not found" });
   }
-  const { page = 1, limit = 50 } = req.query;
+  const { page = 1, limit = 20 } = req.query;
 
   // Pagination logic (optional)
   const skip = (Number(page) - 1) * Number(limit);
+  const query = { chat: chatId };
 
-  const messages = await Message.find({ chat: chatId })
+  const totalMessages = await Message.countDocuments(query);
+
+  const messages = await Message.find(query)
     .sort({ createdAt: -1 })
     .skip(skip)
     .limit(Number(limit));
   return res.status(200).json({
     messages: messages.reverse(),
+    totalMessages: totalMessages,
     page,
+    totalPages: Math.ceil(totalMessages / Number(limit)),
     limit,
-    total: messages.length,
     chat,
   });
 };
