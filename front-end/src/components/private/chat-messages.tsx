282b3a1f6de88f2233c453f720a8f25742e02b1e
kumar-rinku0 <rinkukumar412616@gmail.com>
2026-03-17 08:38:34 +0530

    added send attachment files in chat and show them on ui

--- front-end/src/components/private/chat-messages.tsx
+++ front-end/src/components/private/chat-messages.tsx
@@ -1,9 +1,7 @@
 import api from "@/services/api";
 import type { ChatType, MessageType, UserType } from "@/types/api-types";
-import React, { useEffect, useRef } from "react";
-import { motion } from "motion/react";
-import { easeOut } from "motion"; // Add this import at the top with other imports
-import { MoreVertical, SendHorizonal } from "lucide-react";
+import React, { useEffect } from "react";
+import { MoreVertical } from "lucide-react";
 import socket from "@/services/socket";
 import { useNavigate, useParams } from "react-router";
 import { Button } from "../ui/button";
@@ -16,6 +14,7 @@ import {
 } from "@/components/ui/popover";
 import { toast } from "sonner";
 import AllMessages from "./all-messages";
+import SendMessage from "./send-message";
 
 type ResponseType = {
   messages: MessageType[];
@@ -24,11 +23,6 @@ type ResponseType = {
   totalPages: number;
   limit: number;
   chat: ChatType;
-};
-
-const transitionDebug = {
-  duration: 0.2,
-  ease: easeOut,
 };
 
 const ChatMessages = () => {
@@ -49,7 +43,7 @@ const ChatMsgFunc = ({ chatId, chat }: { chatId: string; chat: ChatType }) => {
     totalPages: number;
     limit: number;
   }>({ totalMessages: 0, page: 1, totalPages: 1, limit: 0 });
-  const [msg, setMsg] = React.useState<string>("");
+
   const auth_user_token = localStorage.getItem("auth_user") || "";
   const auth_user = JSON.parse(auth_user_token) as UserType;
   function fetchChatMessages(page: number) {
@@ -92,49 +86,16 @@ const ChatMsgFunc = ({ chatId, chat }: { chatId: string; chat: ChatType }) => {
     };
   }, [chatId]);
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    // Send the message to the API
-    if (!auth_user || msg.trim() === "") {
-      console.error("No token found in localStorage or empty message");
-      return;
-    }
-    api
-      .post(`/msg/create`, { msg: msg, chatId: chatId, sender: auth_user._id })
-      .then((response) => {
-        // Handle the response if needed
-        const { message } = response.data;
-        setMessages((prevMessages) => [...prevMessages, message]);
-        setMsg(""); // Clear the input field after sending the message
-        if (!chat) return;
-        socket.emit("msg", chat._id, message);
-      });
-  };
-
   const updateMessages = (ids: string[]) => {
     setMessages((prev) => prev.filter((m) => !ids.includes(m._id)));
   };
 
-  const typingTimeoutRef = useRef<NodeJS.Timeout | null>(null);
-
-  const handleTyping = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setMsg(e.target.value);
-    socket.emit("typing", chatId, auth_user._id);
-
-    if (typingTimeoutRef.current) {
-      clearTimeout(typingTimeoutRef.current);
-    }
-
-    typingTimeoutRef.current = setTimeout(() => {
-      socket.emit("stop_typing", chatId, auth_user._id);
-    }, 1000);
-  };
-
   return (
     <div className="h-[100vh]">
+      {/* chat header */}
       <div className="h-14 px-2 flex justify-between items-center border-b border-b-gray-200">
-        <div className="flex flex-col px-4">
-          {/* <div className="relative w-12 h-12">
+        <div className="flex justify-center items-center gap-4 px-4">
+          <div className="relative w-12 h-12">
             <img
               src={
                 chat?.displayAvatar ||
@@ -143,7 +104,7 @@ const ChatMsgFunc = ({ chatId, chat }: { chatId: string; chat: ChatType }) => {
               alt=""
               className="object-cover w-12 h-12 rounded-full"
             />
-          </div> */}
+          </div>
           <span className="font-semibold">{chat?.displayName}</span>
           {typingUsers && typingUsers.some((user) => user?.typing) && (
             <span className="font-light text-sm truncate">
@@ -156,7 +117,7 @@ const ChatMsgFunc = ({ chatId, chat }: { chatId: string; chat: ChatType }) => {
         </div>
         <MoreOptions chatId={chatId!} updateMessages={updateMessages} />
       </div>
-      {/* <div className="flex h-[calc(100vh-40px)] flex-col items-end justify-end pb-4 px-1"> */}
+      {/* messages */}
       <div className="flex relative h-[calc(100vh-3.5rem)] flex-col">
         <AllMessages
           key={chatId}
@@ -166,43 +127,13 @@ const ChatMsgFunc = ({ chatId, chat }: { chatId: string; chat: ChatType }) => {
           fetchChatMessages={fetchChatMessages}
           updateMessages={updateMessages}
         />
-
-        {/* <div className="flex w-full"> */}
-        <form
-          onSubmit={handleSubmit}
-          className="h-12 flex justify-center items-center w-full bg-accent dark:bg-accent-foreground px-1 border-t border-t-neutral-200 dark:border-t-neutral-800"
-        >
-          <input
-            type="text"
-            onChange={handleTyping}
-            value={msg}
-            className="relative h-9 w-[250px] flex-grow rounded-lg border border-gray-200 bg-white px-3 text-[15px] outline-none placeholder:text-gray-400 focus-visible:ring-0 focus-visible:ring-blue-500/20 focus-visible:ring-offset-1
-            dark:border-black/60 dark:bg-black dark:text-gray-50 dark:placeholder-gray-500 dark:focus-visible:ring-blue-500/20 dark:focus-visible:ring-offset-1 dark:focus-visible:ring-offset-gray-700"
-            placeholder="Type your message"
-          />
-          <motion.div
-            key={messages.length}
-            layout="position"
-            className="pointer-events-none absolute z-10 flex h-9 w-[250px] items-center overflow-hidden break-words rounded-full bg-gray-200 [word-break:break-word] dark:bg-black"
-            layoutId={`container-[${messages.length}]`}
-            initial={{ opacity: 0.6, zIndex: -1 }}
-            animate={{ opacity: 0.6, zIndex: -1 }}
-            exit={{ opacity: 1, zIndex: 1 }}
-            transition={transitionDebug}
-          >
-            <div className="px-3 py-2 text-[15px] leading-[15px] text-gray-900 dark:text-gray-50">
-              {msg || "Type your message"}
-            </div>
-          </motion.div>
-          <button
-            type="submit"
-            className="ml-2 flex h-9 w-9 items-center justify-center rounded-full bg-gray-200
-            dark:bg-black"
-          >
-            <SendHorizonal className="h-5 w-5 text-gray-600 dark:text-gray-50" />
-          </button>
-        </form>
-        {/* </div> */}
+        {/* input & send message button */}
+        <SendMessage
+          chatId={chatId}
+          chat={chat}
+          setMessages={setMessages}
+          messages={messages}
+        />
       </div>
     </div>
   );
---
+++ front-end/src/components/private/send-message.tsx
@@ -0,0 +1,230 @@
+import { motion } from "motion/react";
+import { easeOut } from "motion"; // Add this import at the top with other imports
+import { Link, SendHorizonal } from "lucide-react";
+import React from "react";
+import socket from "@/services/socket";
+import type { ChatType, MessageType } from "@/types/api-types";
+import api from "@/services/api";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { Label } from "../ui/label";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { useAuth } from "@/hooks/use-auth";
+
+const transitionDebug = {
+  duration: 0.2,
+  ease: easeOut,
+};
+
+type ResponseTypeOne = {
+  signature: string;
+  timestamp: string;
+  cloudName: string;
+  apiKey: string;
+};
+
+const getSignature = async (path: string) => {
+  const data = await api
+    .get<ResponseTypeOne>(`/user/cloud-sign?origin=chats&path=${path}`)
+    .then((res) => res.data);
+  return data;
+};
+
+const SendMessage = ({
+  chatId,
+  chat,
+  setMessages,
+  messages,
+}: {
+  chatId: string;
+  chat: ChatType;
+  setMessages: React.Dispatch<React.SetStateAction<MessageType[]>>;
+  messages: MessageType[];
+}) => {
+  const auth_user_token = localStorage.getItem("auth_user") || "";
+  const auth_user = JSON.parse(auth_user_token);
+  const [msg, setMsg] = React.useState<string>("");
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // Send the message to the API
+    if (!auth_user || msg.trim() === "") {
+      console.error("No token found in localStorage or empty message");
+      return;
+    }
+    api
+      .post(`/msg/create`, { msg: msg, chatId: chatId, sender: auth_user._id })
+      .then((response) => {
+        // Handle the response if needed
+        const { message } = response.data;
+        setMessages((prevMessages) => [...prevMessages, message]);
+        setMsg(""); // Clear the input field after sending the message
+        if (!chat) return;
+        socket.emit("msg", chat._id, message);
+      });
+  };
+  const typingTimeoutRef = React.useRef<NodeJS.Timeout | null>(null);
+
+  const handleTyping = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setMsg(e.target.value);
+    socket.emit("typing", chatId, auth_user._id);
+
+    if (typingTimeoutRef.current) {
+      clearTimeout(typingTimeoutRef.current);
+    }
+
+    typingTimeoutRef.current = setTimeout(() => {
+      socket.emit("stop_typing", chatId, auth_user._id);
+    }, 1000);
+  };
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="h-12 flex justify-center items-center w-full bg-accent dark:bg-accent-foreground px-1 border-t border-t-neutral-200 dark:border-t-neutral-800"
+    >
+      <Input
+        type="text"
+        onChange={handleTyping}
+        value={msg}
+        className="relative h-9 w-[250px] flex-grow rounded-lg border border-gray-200 bg-white px-3 text-[15px] outline-none placeholder:text-gray-400 focus-visible:ring-0 focus-visible:ring-blue-500/20 focus-visible:ring-offset-1
+            dark:border-black/60 dark:bg-black dark:text-gray-50 dark:placeholder-gray-500 dark:focus-visible:ring-blue-500/20 dark:focus-visible:ring-offset-1 dark:focus-visible:ring-offset-gray-700"
+        placeholder="Type your message"
+      />
+      <motion.div
+        key={messages.length}
+        layout="position"
+        className="pointer-events-none absolute z-10 flex h-9 w-[250px] items-center overflow-hidden break-words rounded-full bg-gray-200 [word-break:break-word] dark:bg-black"
+        layoutId={`container-[${messages.length}]`}
+        initial={{ opacity: 0.6, zIndex: -1 }}
+        animate={{ opacity: 0.6, zIndex: -1 }}
+        exit={{ opacity: 1, zIndex: 1 }}
+        transition={transitionDebug}
+      >
+        <div className="px-3 py-2 text-[15px] leading-[15px] text-gray-900 dark:text-gray-50">
+          {msg || "Type your message"}
+        </div>
+      </motion.div>
+
+      <SendMessageWithAttachment chatId={chatId} setMessages={setMessages} />
+
+      <Button
+        type="submit"
+        className="ml-2 flex h-9 w-9 items-center justify-center rounded-full bg-gray-200
+            dark:bg-black cursor-pointer"
+      >
+        <SendHorizonal className="h-5 w-5 text-gray-600 dark:text-gray-50" />
+      </Button>
+    </form>
+  );
+};
+
+const SendMessageWithAttachment = ({
+  chatId,
+  setMessages,
+}: {
+  chatId: string;
+  setMessages: React.Dispatch<React.SetStateAction<MessageType[]>>;
+}) => {
+  const { authInfo } = useAuth();
+  if (!authInfo) return;
+
+  const [files, setFiles] = React.useState<File[] | null>(null);
+  const [loading, setLoading] = React.useState(false);
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFiles(e.target.files ? Array.from(e.target.files) : null);
+  };
+  const handleUploadFiles = async () => {
+    if (!files || files.length < 1) return;
+    setLoading(true);
+    for (const file of files) {
+      const { signature, timestamp, cloudName, apiKey } =
+        await getSignature(chatId);
+      const data = new FormData();
+      data.append("file", file);
+      data.append("api_key", apiKey); // safe, can be public
+      data.append("timestamp", timestamp);
+      data.append("signature", signature);
+      data.append("folder", `chats/${chatId}`);
+      const response = await fetch(
+        `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`,
+        { method: "POST", body: data },
+      );
+      const result = await response.json();
+      console.log("cloudinary res result", result);
+      const { data: mData } = await api.post<{
+        message: MessageType;
+        ok: boolean;
+      }>(`/msg/create`, {
+        chatId: chatId,
+        sender: authInfo.auth_user._id,
+        msg: result.original_filename as string,
+        attachment: {
+          url: result.secure_url as string,
+          type: file.type ?? "application/octet-stream",
+        },
+      });
+      console.log(mData);
+      if (!mData.ok) {
+        console.log(mData.message);
+        return;
+      }
+      setMessages((prev) => [...prev, mData.message]);
+    }
+    setLoading(false);
+    setFiles(null);
+  };
+  return (
+    <>
+      <Input
+        type="file"
+        id="file-upload"
+        className="hidden"
+        onChange={handleFileChange}
+        multiple
+      />
+
+      <Label
+        htmlFor="file-upload"
+        className="ml-2 flex h-9 w-9 cursor-pointer items-center justify-center rounded-full bg-gray-200
+          dark:bg-black"
+      >
+        <Link className="h-4 w-4 text-gray-600 dark:text-gray-50" />
+      </Label>
+
+      <Dialog open={!!files} onOpenChange={() => setFiles(null)}>
+        <DialogContent showCloseButton={false}>
+          <DialogHeader>
+            <DialogTitle>Are you absolutely sure?</DialogTitle>
+
+            {files && files.map((file) => <span>{file.name}</span>)}
+
+            <DialogDescription>
+              This will save file data to our servers. You can remove that
+              later.
+            </DialogDescription>
+          </DialogHeader>
+          <DialogFooter>
+            <Button
+              variant="outline"
+              onClick={() => setFiles(null)}
+              disabled={loading}
+            >
+              Cancel
+            </Button>
+            <Button onClick={handleUploadFiles} disabled={loading}>
+              Yes, I'm sure
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+};
+
+export default SendMessage;
